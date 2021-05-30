a34a072d082c0677a2d9c34ab1f30e9dbd3667ed
sajedul <sajedulkhairul@gmail.com>
2021-05-30 09:34:57 +0600

    CRUD full project

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -35,7 +35,7 @@
   
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-info">Register Student</button>
+      <button type="submit" class="btn btn-info">UpDate Student</button>
     </div>
   </div>
 </form>
